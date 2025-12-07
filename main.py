48da0fe480505fda92a24a5c8960f73af5218cff
Jerin Joice <jerinjoice18@gmail.com>
2025-12-07 13:27:43 +0530

    Basic program for working in terminal

---
+++ main.py
@@ -0,0 +1,20 @@
+def printlist(tasks):
+    i=1
+    for task in tasks:
+        print(f"{i}:{task}")
+        i+=1
+  
+print("Welcome to the To Do List App!")
+tasks=[]
+x=True
+while(x):
+    print("Enter 1 to view tasks\nEnter 2 to add a task\nEnter 3 to exit")
+    choice=int(input("Enter a choice:"))
+    if choice==1:
+        printlist(tasks)
+    if choice==2:
+        toappend=input("Enter the new task:")
+        tasks.append(toappend)
+    if choice==3:
+        print("Program was exited")
+        x=False
